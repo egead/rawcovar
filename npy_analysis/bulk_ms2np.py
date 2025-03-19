6292028e42d55b8255cca5095f6fef472c8b48f7
Ege <egeisxol@gmail.com>
2025-03-19 15:10:05 +0300

    Added: Better data handling, only valid data is added and no padding in mseed2npy. Also other fixes

--- npy_analysis/bulk_ms2np.py
+++ npy_analysis/bulk_ms2np.py
@@ -1,32 +1,18 @@
 import os
 from os.path import join as opj
-from pathlib import Path
 from mseed2npy import ms2np
 
-folder_path='/home/boxx/Public/earthquake_model_evaluations/data/SilivriPaper_2019-09-01__2019-11-30'
-output_path='./data/silivri/'
+folder_path='/home/ege/rawcovar/ADVT/'
+output_dir='/home/ege/rawcovar/ADVT_NPYS/'
 
-prepared_waveforms_path=opj(folder_path,'prepared_waveforms')
-prepared_wfs_daily_path = opj(prepared_waveforms_path,'day_by_day')
-
-source_path=prepared_wfs_daily_path
+source_path=folder_path
 file_list = []
 
 for root, dirs, files in os.walk(source_path):
     for filename in files: 
+        base,ext=os.path.splitext(filename)
         if ext=='.mseed':
             file_path=opj(root,filename)
-
-            rel_path=os.path.relpath(root,source_path)
-            out_folder=opj(output_path,rel_path)
             file_list.append(file_path)
 
-            os.makedirs(out_folder,exist_ok=True)
-            new_filename= base+'_processed'+'.npy'
-            save_path=opj(out_folder,new_filename)
-            base,ext=os.path.splitext(filename)
-
-            #   I need to modify ms2np so that it takes the merged_stream object. 
-            # A new function that takes the file_list and output_path would suffice ? 
-
-            ms2np(stream_path=file_path, save_path=save_path)
+        ms2np(file_list=file_list, output_dir=output_dir)
